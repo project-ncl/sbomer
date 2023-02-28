6c1549dda5f4d45d75efd59e8d76afecb09533ca
Andrea Vibelli <avibelli@redhat.com>
2023-02-28 20:43:17 +0100

    Throw 404 if no result is found

--- sbomer/src/test/java/org/redhat/sbomer/test/TestSBOMService.java
+++ sbomer/src/test/java/org/redhat/sbomer/test/TestSBOMService.java
@@ -18,13 +18,11 @@
 package org.redhat.sbomer.test;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.util.Iterator;
-import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.redhat.sbomer.dto.BaseSBOM;
 import org.redhat.sbomer.dto.response.Page;
@@ -32,6 +30,11 @@ import org.redhat.sbomer.service.SBOMService;
 
 import io.quarkus.test.junit.QuarkusTest;
 import lombok.extern.slf4j.Slf4j;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @QuarkusTest
 @Slf4j
@@ -46,7 +49,7 @@ public class TestSBOMService {
     public void testGetBaseSbom() throws IOException {
         log.info("testGetBaseSbom ...");
         BaseSBOM baseSBOM = sbomService.getBaseSbom(INITIAL_BUILD_ID);
-        Assertions.assertNotNull(baseSBOM);
+        assertNotNull(baseSBOM);
     }
 
     @Test
@@ -54,11 +57,11 @@ public class TestSBOMService {
         log.info("testListBaseSboms ...");
 
         Page<BaseSBOM> page = sbomService.listBaseSboms(0, 50);
-        Assertions.assertEquals(0, page.getPageIndex());
-        Assertions.assertEquals(50, page.getPageSize());
-        Assertions.assertTrue(page.getTotalHits() > 0);
-        Assertions.assertEquals(1, page.getTotalPages());
-        Assertions.assertTrue(page.getContent().size() > 0);
+        assertEquals(0, page.getPageIndex());
+        assertEquals(50, page.getPageSize());
+        assertTrue(page.getTotalHits() > 0);
+        assertEquals(1, page.getTotalPages());
+        assertTrue(page.getContent().size() > 0);
 
         boolean found = false;
         Iterator<BaseSBOM> contentIterator = page.getContent().iterator();
@@ -69,6 +72,16 @@ public class TestSBOMService {
                 return;
             }
         }
-        Assertions.assertTrue(found);
+        assertTrue(found);
+    }
+
+    @Test
+    public void testBaseSbomNotFound() throws IOException {
+        log.info("testBaseSbomNotFound ...");
+        try {
+            BaseSBOM baseSBOM = sbomService.getBaseSbom("I_DO_NOT_EXIST");
+            fail("It should have thrown a 404 exception");
+        } catch (NotFoundException nfe) {
+        }
     }
 }
