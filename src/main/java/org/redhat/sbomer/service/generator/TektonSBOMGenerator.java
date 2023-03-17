344e1c523dd7a5ae7f761c4b3ad2fe5d17546e73
Marek Goldmann <mgoldman@redhat.com>
2023-03-17 13:39:43 +0100

    feat: use dedicated sa for tekton resources

--- src/main/java/org/redhat/sbomer/service/generator/TektonSBOMGenerator.java
+++ src/main/java/org/redhat/sbomer/service/generator/TektonSBOMGenerator.java
@@ -50,7 +50,7 @@ public class TektonSBOMGenerator implements SBOMGenerator {
                 .withGenerateName("sbom-")
                 .endMetadata()
                 .withNewSpec()
-                .withServiceAccountName("sbomer-sa")
+                .withServiceAccountName("sbomer-tekton-sa")
                 .withNewPipelineRef()
                 .withName("sbom-generator-domino")
                 .endPipelineRef()
