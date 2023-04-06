6596abf232a9bd6afdb89af865a8cbe9fa6e8d89
Andrea Vibelli <avibelli@redhat.com>
2023-04-06 12:06:04 +0200

    Add UMB receiver and tests for sending and receiving UMB messages of PNC completed builds

---
+++ service/src/main/java/org/jboss/sbomer/messaging/PNCMessageParser.java
@@ -0,0 +1,157 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.sbomer.messaging;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jboss.pnc.api.enums.BuildStatus;
+import org.jboss.pnc.api.enums.ProgressStatus;
+import org.jboss.pnc.common.Strings;
+import org.jboss.sbomer.core.enums.GeneratorImplementation;
+import org.jboss.sbomer.service.SBOMService;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Session;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PNCMessageParser implements Runnable {
+
+    private static final ObjectMapper msgMapper = new ObjectMapper();
+
+    static {
+        msgMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        msgMapper.registerModule(new JavaTimeModule()).configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        msgMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        msgMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        msgMapper.disable(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS);
+    }
+
+    private AtomicBoolean shouldRun = new AtomicBoolean(false);
+    private AtomicBoolean connected = new AtomicBoolean(false);
+    private AtomicInteger receivedMessages = new AtomicInteger(0);
+    private ConnectionFactory cf;
+    private SBOMService sbomService;
+    private String incomingTopic;
+    private Message lastMessage;
+
+    public PNCMessageParser(ConnectionFactory cf, String incomingTopic, SBOMService sbomService) {
+        this.cf = cf;
+        this.incomingTopic = incomingTopic;
+        this.sbomService = sbomService;
+        this.shouldRun.set(true);
+    }
+
+    @Override
+    public void run() {
+        log.info("Listening on topic: {}", incomingTopic);
+
+        try (JMSContext context = cf.createContext(Session.AUTO_ACKNOWLEDGE)) {
+            log.info("JMS client ID {}.", context.getClientID());
+
+            JMSConsumer consumer = context.createConsumer(context.createQueue(incomingTopic));
+            while (shouldRun.get()) {
+                connected.set(true);
+                Message message = consumer.receive();
+                if (message == null) {
+                    // receive returns `null` if the JMSConsumer is closed
+                    return;
+                }
+
+                lastMessage = message;
+                receivedMessages.incrementAndGet();
+
+                try {
+
+                    JsonNode msgBody = JmsUtils.getLastMsgBody(lastMessage);
+                    if (msgBody == null) {
+                        continue;
+                    }
+                    if (shouldGenerateSBOM(msgBody)) {
+                        String buildId = msgBody.path("build").path("id").asText();
+                        if (!Strings.isEmpty(buildId)) {
+                            log.info("I SHOULD REALLY GENERATE AN SBOM FOR BUILD {}", buildId);
+                            sbomService.generateSbomFromPncBuild(buildId, GeneratorImplementation.CYCLONEDX);
+                        }
+                    }
+                } catch (JMSException | IOException e) {
+                    log.error(
+                            "Cannot convert UMB message {} from topic {} to Json",
+                            message.getJMSMessageID(),
+                            message.getJMSDestination(),
+                            e);
+                }
+
+            }
+        } catch (Exception e) {
+            log.error("Something wrong happened in the PNCMessageParser", e);
+        }
+
+        connected.set(false);
+    }
+
+    public boolean shouldRun() {
+        return shouldRun.get();
+    }
+
+    public void setShouldRun(boolean shouldRun) {
+        this.shouldRun.set(shouldRun);
+    }
+
+    public boolean isConnected() {
+        return connected.get();
+    }
+
+    public Message getLastMessage() {
+        return lastMessage;
+    }
+
+    public int getReceivedMessages() {
+        return receivedMessages.get();
+    }
+
+    public boolean shouldGenerateSBOM(JsonNode msgBody) {
+
+        JsonNode buildNode = msgBody.path("build");
+        Boolean persistent = !buildNode.path("temporaryBuild").asBoolean();
+        String status = buildNode.path("status").asText();
+        String progress = msgBody.path("build").path("progress").asText();
+
+        if (persistent && ProgressStatus.FINISHED.name().equals(progress) && (BuildStatus.SUCCESS.name().equals(status)
+                || BuildStatus.NO_REBUILD_REQUIRED.name().equals(status))) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ service/src/test/java/org/jboss/sbomer/test/messaging/PncBuildTest.java
@@ -0,0 +1,143 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.sbomer.test.messaging;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import org.jboss.pnc.api.enums.BuildStatus;
+import org.jboss.pnc.api.enums.ProgressStatus;
+import org.jboss.sbomer.messaging.JmsUtils;
+import org.jboss.sbomer.messaging.UmbMessageConsumer;
+import org.jboss.sbomer.test.TestResources;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.quarkus.artemis.test.ArtemisTestResource;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import lombok.extern.slf4j.Slf4j;
+
+@QuarkusTest
+@QuarkusTestResource(ArtemisTestResource.class)
+@TestMethodOrder(OrderAnnotation.class)
+@Slf4j
+public class PncBuildTest {
+
+    @Inject
+    UmbMessageConsumer msgConsumer;
+
+    @Inject
+    ConnectionFactory connectionFactory;
+
+    @Test
+    @Order(1)
+    public void testUMBProducerPNCBuild() throws Exception {
+        log.info("Running testUMBProducerPNCBuild...");
+
+        try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
+            TextMessage txgMsg = preparePNCBuildMsg(context);
+            context.createProducer()
+                    .send(context.createQueue("Consumer.pncsbomer.testing.VirtualTopic.eng.pnc.>"), txgMsg);
+        }
+    }
+
+    @Test
+    @Order(2)
+    public void testUMBConsumerPNCBuild() throws Exception {
+        log.info("Running testUMBConsumerPNCBuild...");
+
+        assertTrue(Wait.waitFor(() -> {
+            Message msg = msgConsumer.getLastMessageReceived();
+            return msg != null;
+        }, 10000, 25), "PNC Build message did not became available in allotted time");
+
+        Message msg = msgConsumer.getLastMessageReceived();
+        String producer = msg.getStringProperty("producer");
+        assertEquals("PNC", producer);
+
+        JsonNode msgBody = JmsUtils.getLastMsgBody(msg);
+        JsonNode buildNode = msgBody.path("build");
+
+        Boolean persistent = !buildNode.path("temporaryBuild").asBoolean();
+        String status = buildNode.path("status").asText();
+        String progress = msgBody.path("build").path("progress").asText();
+        String buildId = msgBody.path("build").path("id").asText();
+
+        assertEquals(true, persistent);
+        assertEquals(BuildStatus.SUCCESS.name(), status);
+        assertEquals(ProgressStatus.FINISHED.name(), progress);
+        assertEquals("AX5TJMYHQAIAE", buildId);
+
+        assertTrue(
+                persistent && ProgressStatus.FINISHED.name().equals(progress)
+                        && (BuildStatus.SUCCESS.name().equals(status)
+                                || BuildStatus.NO_REBUILD_REQUIRED.name().equals(status)));
+
+    }
+
+    private TextMessage preparePNCBuildMsg(JMSContext context) throws IOException {
+
+        // Rebuilding mesg
+        // https://datagrepper.engineering.redhat.com/id?id=ID:orch-70-z4tx6-43917-1678809685060-25:1:8557:1:1&is_raw=true&size=extra-large
+        Map<String, String> headers = new HashMap<>();
+        headers.put("type", "BuildStateChange");
+        headers.put("attribute", "state-change");
+        headers.put("name", "org.kie-kie-jpmml-integration-7.67.0.Final-7.13.3");
+        headers.put("configurationId", "10884");
+        headers.put("configurationRevision", "2653483");
+        headers.put("oldStatus", "BUILDING");
+        headers.put("newStatus", "SUCCESS");
+        headers.put("JMSXUserID", "projectnewcastle");
+        headers.put("amq6100_originalDestination", "topic://VirtualTopic.eng.pnc.builds");
+        headers.put("correlationId", "a420416c-d184-4ced-9277-500667305139");
+        headers.put("destination", "/topic/VirtualTopic.eng.pnc.builds");
+        headers.put("messageId", "ID:orch-70-z4tx6-43917-1678809685060-25:1:8557:1:1");
+        headers.put("persistent", "true");
+        headers.put("producer", "PNC");
+
+        String msgBody = TestResources.asString("payloads/umb-pnc-build-body.json");
+        TextMessage textMessage = context.createTextMessage(msgBody);
+
+        headers.forEach((k, v) -> {
+            try {
+                textMessage.setStringProperty(k, v);
+            } catch (JMSException e) {
+                log.error("Error while setting JMS headers", e);
+            }
+        });
+
+        return textMessage;
+    }
+
+}
