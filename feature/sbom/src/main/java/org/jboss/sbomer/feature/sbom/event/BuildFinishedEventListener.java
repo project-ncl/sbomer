66cb84767f1c216ae9740787f351197f018f5891
Marek Goldmann <mgoldman@redhat.com>
2023-05-31 14:06:06 +0200

    feat: add license headers, disable JSON logging for sbom feature

--- feature/sbom/src/main/java/org/jboss/sbomer/feature/sbom/event/BuildFinishedEventListener.java
+++ feature/sbom/src/main/java/org/jboss/sbomer/feature/sbom/event/BuildFinishedEventListener.java
@@ -1,3 +1,20 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.sbomer.feature.sbom.event;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -8,7 +25,6 @@ import org.jboss.sbomer.feature.sbom.k8s.model.GenerationRequest;
 import org.jboss.sbomer.feature.sbom.k8s.model.GenerationRequestBuilder;
 import org.jboss.sbomer.feature.sbom.k8s.model.SbomGenerationStatus;
 
-import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.quarkus.runtime.StartupEvent;
 
@@ -25,8 +41,6 @@ public class BuildFinishedEventListener {
 				.withBuildId("AABBCC")
 				.withStatus(SbomGenerationStatus.NEW)
 				.build();
-
-		System.out.println(req);
 
 		// ConfigMap cm = kubernetesClient.configMaps().resource(req).create();
 
