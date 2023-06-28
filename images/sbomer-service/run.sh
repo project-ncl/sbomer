08ea1e005b7e5565af4169f1930b4ca24e99f6cd
Andrea Vibelli <avibelli@redhat.com>
2023-06-28 14:53:22 +0200

    Fix typo

--- images/sbomer-service/run.sh
+++ images/sbomer-service/run.sh
@@ -22,7 +22,7 @@ set -x
 set -e
 
 if [ -f "/mnt/secrets/env.sh" ]; then
-    cp /mnt/secrets/env.sh /deplyoments/run-env.sh
+    cp /mnt/secrets/env.sh /deployments/run-env.sh
 fi
 
 exec /usr/local/s2i/run
