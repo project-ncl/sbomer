5ee2d6c6f01ff356c11e431fbf8d13e23c056490
Marek Goldmann <mgoldman@redhat.com>
2023-08-03 14:06:24 +0200

    feat: introduce a new generator framework
    This should help write tests and make execution of them faster.

--- cli/src/main/java/org/jboss/sbomer/cli/feature/sbom/command/MavenDominoGenerateCommand.java
+++ cli/src/main/java/org/jboss/sbomer/cli/feature/sbom/command/MavenDominoGenerateCommand.java
@@ -17,15 +17,9 @@
  */
 package org.jboss.sbomer.cli.feature.sbom.command;
 
-import java.io.IOException;
-import java.lang.ProcessBuilder.Redirect;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
-import org.jboss.sbomer.cli.feature.sbom.config.DefaultGenerationConfig.DefaultGeneratorConfig;
-import org.jboss.sbomer.core.errors.ApplicationException;
+import org.jboss.sbomer.cli.feature.sbom.generate.MavenDominoGenerator;
 import org.jboss.sbomer.core.features.sbom.enums.GeneratorType;
 
 import lombok.extern.slf4j.Slf4j;
@@ -50,28 +44,6 @@ public class MavenDominoGenerateCommand extends AbstractMavenGenerateCommand {
             converter = PathConverter.class)
     Path dominoDir;
 
-    private Path getDominoPath() {
-        if (dominoDir == null) {
-            throw new ApplicationException(
-                    "Directory containing the Domino tool was not provided, please use the --domino-dir option or set it via SBOMER_DOMINO_DIR environment variable");
-        }
-
-        Path dominoPath = dominoDir;
-        String version = toolVersion();
-
-        if (version != null) {
-            dominoPath = dominoPath.resolve(String.format("domino-%s.jar", version));
-        } else {
-            dominoPath = dominoPath.resolve("domino.jar");
-        }
-
-        if (!Files.exists(dominoPath)) {
-            throw new ApplicationException("Domino could not be found on path '{}'", dominoPath.toAbsolutePath());
-        }
-
-        return dominoPath;
-    }
-
     @Override
     protected GeneratorType generatorType() {
         return GeneratorType.MAVEN_DOMINO;
@@ -79,60 +51,16 @@ public class MavenDominoGenerateCommand extends AbstractMavenGenerateCommand {
 
     @Override
     public Path doGenerate(String buildCmdOptions) {
-        Path dominoPath = getDominoPath();
+        MavenDominoGenerator generator = MavenDominoGenerator.builder()
+                .withDominoDir(dominoDir)
+                .withDominoVersion(toolVersion())
+                .build();
 
-        ProcessBuilder processBuilder = new ProcessBuilder().redirectOutput(Redirect.INHERIT)
-                .redirectError(Redirect.INHERIT);
+        log.info("Starting SBOM generation using Domino");
 
-        processBuilder.command(
-                "java",
-                "-Xms256m",
-                "-Xmx512m",
-                // Workaround for Domino trying to parse what it shouldn't parse
-                "-Dquarkus.args=\"\"",
-                "-jar",
-                dominoPath.toAbsolutePath().toString(),
-                "report",
-                String.format("--project-dir=%s", parent.getWorkdir().toAbsolutePath().toString()),
-                String.format("--output-file=%s", BOM_FILE_NAME),
-                "--manifest");
+        generator.generate(parent.getWorkdir(), generatorArgs().split(" "));
 
-        log.info("Working directory: '{}'", parent.getWorkdir());
-        processBuilder.directory(parent.getWorkdir().toFile());
-
-        if (settingsXmlPath != null) {
-            log.debug("Using provided Maven settings.xml configuration file located at '{}'", settingsXmlPath);
-            processBuilder.command().add("-s");
-            processBuilder.command().add(settingsXmlPath.toString());
-        }
-
-        String args = generatorArgs();
-        processBuilder.command().addAll(Arrays.asList(args.split(" ")));
-
-        log.info(
-                "Starting SBOM generation using Domino '{}' with command: '{}'",
-                dominoPath.toAbsolutePath(),
-                processBuilder.command().stream().collect(Collectors.joining(" ")));
-
-        Process process = null;
-
-        try {
-            process = processBuilder.start();
-        } catch (IOException e) {
-            throw new ApplicationException("Error while running the command", e);
-        }
-
-        int exitCode = -1;
-
-        try {
-            exitCode = process.waitFor();
-        } catch (InterruptedException e) {
-            throw new ApplicationException("Unable to obtain the status for the process", e);
-        }
-
-        if (exitCode != 0) {
-            throw new ApplicationException("SBOM generation failed, see logs above");
-        }
+        log.info("SBOM generation finished");
 
         return Path.of(getParent().getWorkdir().toAbsolutePath().toString(), BOM_FILE_NAME);
     }
