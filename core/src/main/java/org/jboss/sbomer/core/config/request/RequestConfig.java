dd4f1c9bd831ede5faf4472f90a95188adef3eb8
Marek Goldmann <mgoldman@redhat.com>
2024-10-31 12:20:31 +0100

    feat: introduction of RequestConfig and subclasses

---
+++ core/src/main/java/org/jboss/sbomer/core/config/request/RequestConfig.java
@@ -0,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.sbomer.core.config.request;
+
+import org.jboss.sbomer.core.errors.ApplicationException;
+import org.jboss.sbomer.core.features.sbom.utils.ObjectMapperProvider;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@SuperBuilder(setterPrefix = "with")
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({ @JsonSubTypes.Type(ImageRequestConfig.class) })
+public abstract class RequestConfig {
+    /**
+     * The API version of the configuration file. In case of breaking changes this value will be used to detect the
+     * correct (de)serializer.
+     */
+    @Builder.Default
+    String apiVersion = "sbomer.jboss.org/v1alpha1";
+
+    public String toJson() {
+        try {
+            return ObjectMapperProvider.json().writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new ApplicationException("Cannot serialize configuration into a JSON string", e);
+        }
+    }
+}
