451f9590fb9a2be21825dc8cdbe5de89db19386e
Tomas Havlicek <thavlice@redhat.com>
2025-03-04 12:18:35 +0100

    feat(SBOMER-341): add select and search bar for requests page

--- ui/src/app/api/DefaultSbomerApi.ts
+++ ui/src/app/api/DefaultSbomerApi.ts
@@ -17,7 +17,15 @@
 ///
 
 import axios, { Axios, AxiosError } from 'axios';
-import { SbomerApi, SbomerGeneration, SbomerManifest, SbomerStats, SbomerRequest, SbomerRequestManifest } from '../types';
+import {
+  RequestsQueryType,
+  SbomerApi,
+  SbomerGeneration,
+  SbomerManifest,
+  SbomerRequest,
+  SbomerRequestManifest,
+  SbomerStats,
+} from '../types';
 
 type Options = {
   baseUrl: string;
@@ -182,21 +190,55 @@ export class DefaultSbomerApi implements SbomerApi {
   }
 
   async getGeneration(id: string): Promise<SbomerGeneration> {
-    const request = await this.client
-      .get<SbomerGeneration>(`/api/v1beta1/generations/${id}`)
-      .then((response) => {
-        return response.data as SbomerGeneration;
-      });
+    const request = await this.client.get<SbomerGeneration>(`/api/v1beta1/generations/${id}`).then((response) => {
+      return response.data as SbomerGeneration;
+    });
 
     return request;
   }
 
-  async getRequestEvents(pagination: {
-    pageSize: number;
-    pageIndex: number;
-  }): Promise<{ data: SbomerRequest[]; total: number }> {
+  async getRequestEvents(
+    pagination: {
+      pageSize: number;
+      pageIndex: number;
+    },
+    queryType: string,
+    query: string,
+  ): Promise<{ data: SbomerRequest[]; total: number }> {
+    let queryPrefix = '';
+    switch (queryType) {
+      case RequestsQueryType.PNCBuild:
+        queryPrefix = 'pnc-build';
+        break;
+      case RequestsQueryType.ContainerImage:
+        queryPrefix = 'image';
+        break;
+      case RequestsQueryType.ErrataAdvisory:
+        queryPrefix = 'errata-advisory';
+        break;
+      case RequestsQueryType.RequestEvent:
+        queryPrefix = 'id';
+        break;
+      case RequestsQueryType.PNCAnalysis:
+        queryPrefix = 'pnc-analysis';
+        break;
+      case RequestsQueryType.PNCOperation:
+        queryPrefix = 'pnc-operation';
+        break;
+      case RequestsQueryType.ErrataReleaseID:
+        queryPrefix = 'release.errata_id';
+        break;
+      case RequestsQueryType.ErrataReleaseFullname:
+        queryPrefix = 'release.errata_fullname';
+        break;
+      default:
+        queryPrefix = '';
+    }
+
+    const queryFullString = queryPrefix == '' ? '' : `${queryPrefix}=${query}`;
+
     const response = await fetch(
-      `${this.baseUrl}/api/v1beta1/requests?pageSize=${pagination.pageSize}&pageIndex=${pagination.pageIndex}`,
+      `${this.baseUrl}/api/v1beta1/requests/${encodeURIComponent(queryFullString)}?pageSize=${pagination.pageSize}&pageIndex=${pagination.pageIndex}`,
     );
 
     if (response.status != 200) {
@@ -212,20 +254,24 @@ export class DefaultSbomerApi implements SbomerApi {
     const requests: SbomerRequest[] = [];
 
     if (data.content) {
+      // basic response without any filters applied
       data.content.forEach((request: any) => {
         requests.push(new SbomerRequest(request));
       });
+      return { data: requests, total: data.totalHits };
     }
 
-    return { data: requests, total: data.totalHits };
+    // filtered response has different format than normal
+    data.forEach((request: any) => {
+      requests.push(new SbomerRequest(request));
+    });
+    return { data: requests, total: requests.length || 0 };
   }
 
   async getRequestEvent(id: string): Promise<SbomerRequestManifest> {
-    const request = await this.client
-      .get<SbomerRequestManifest>(`/api/v1beta1/requests/id=${id}`)
-      .then((response) => {
-        return new SbomerRequestManifest(response.data[0]);
-      });
+    const request = await this.client.get<SbomerRequestManifest>(`/api/v1beta1/requests/id=${id}`).then((response) => {
+      return new SbomerRequestManifest(response.data[0]);
+    });
 
     return request;
   }
@@ -274,5 +320,4 @@ export class DefaultSbomerApi implements SbomerApi {
 
     return { data: requests, total: totalHits };
   }
-
 }
