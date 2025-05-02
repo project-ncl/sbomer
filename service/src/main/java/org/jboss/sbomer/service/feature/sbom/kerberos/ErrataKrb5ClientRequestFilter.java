db0dee4e404e9006c147cf534c35f5eed9229235
Jonathan Christison <jochrist@redhat.com>
2025-05-02 15:14:24 +0100

    fix(kerberos): remove static keyword and use annotation instead
    In this commit:
    
    * Remove static keyword from the enable bool
    * Switch to using `@ConfigProperty` annotation for readability

--- service/src/main/java/org/jboss/sbomer/service/feature/sbom/kerberos/ErrataKrb5ClientRequestFilter.java
+++ service/src/main/java/org/jboss/sbomer/service/feature/sbom/kerberos/ErrataKrb5ClientRequestFilter.java
@@ -19,7 +19,7 @@ package org.jboss.sbomer.service.feature.sbom.kerberos;
 
 import java.io.IOException;
 
-import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.client.ClientRequestContext;
@@ -35,13 +35,14 @@ public class ErrataKrb5ClientRequestFilter implements ClientRequestFilter {
     @Inject
     ErrataCachingKerberosClientSupport kerberosClientSupport;
 
-    private static boolean enabled = ConfigProvider.getConfig()
-            .getValue("sbomer.features.kerberos.enabled", Boolean.class);
+    @ConfigProperty(name = "sbomer.features.kerberos.enabled", defaultValue = "false")
+    private boolean enabled;
 
     @Override
     public void filter(ClientRequestContext requestContext) throws IOException {
         if (!enabled)
             return;
+            
         String serviceTicket = kerberosClientSupport.getServiceTicket();
         requestContext.getHeaders().add(AUTHORIZATION, NEGOTIATE + " " + serviceTicket);
     }
