abf0350af881bd144e84c3640b88ae928a347bf8
Marek Goldmann <marek.goldmann@gmail.com>
2025-06-03 10:31:18 +0200

    feat: make syft controller working

--- service/src/main/java/org/jboss/sbomer/service/v1beta2/controller/Controller.java
+++ service/src/main/java/org/jboss/sbomer/service/v1beta2/controller/Controller.java
@@ -15,19 +15,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.sbomer.service.rest.api.v1beta2.payloads.generation;
+package org.jboss.sbomer.service.v1beta2.controller;
 
-import java.util.Map;
+import java.util.Set;
 
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import io.fabric8.tekton.v1beta1.TaskRun;
 
-/**
- * Options for a given generator used for generating manifests.
- *
- * @param format The manifest output format.
- * @param resources Resource requirements related to execution phase for a current generation.
- * @param options Custom, generator(version)-specific, options which should be applied to the generation process.
- */
-public record GeneratorConfigSpec(String format, ResourcesSpec resources,
-        @Schema(description = "Specific options for a particular generator version.") Map<String, Object> options) {
+public interface Controller<R> {
+    void reconcile(R resource, Set<TaskRun> relatedTaskRuns);
 }
