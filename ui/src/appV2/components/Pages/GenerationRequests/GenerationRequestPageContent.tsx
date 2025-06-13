22c1103858e8a66ab9c5db77bb7ca0078925a3b8
Tomas Havlicek <thavlice@redhat.com>
2025-06-13 16:37:55 +0200

    feat: make json raw info better looking

--- ui/src/appV2/components/Pages/GenerationRequests/GenerationRequestPageContent.tsx
+++ ui/src/appV2/components/Pages/GenerationRequests/GenerationRequestPageContent.tsx
@@ -13,6 +13,7 @@ import {
   TimestampTooltipVariant,
   Title,
   Alert,
+  PageSection,
 } from '@patternfly/react-core';
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
@@ -41,48 +42,43 @@ const GenerationRequestPageContent: React.FunctionComponent = () => {
   }
 
   return (
-    <Grid hasGutter span={12}>
-      <GridItem span={12}>
-        <Title headingLevel="h1" size="4xl">
-          Generation Request {id}
-        </Title>
-      </GridItem>
-      <GridItem span={12}>
-        <DescriptionList columnModifier={{ default: '2Col' }}>
-          <DescriptionListGroup>
-            <DescriptionListTerm>Generation Request ID</DescriptionListTerm>
-            <DescriptionListDescription>
-              <pre>{id}</pre>
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-          <DescriptionListGroup>
-            <DescriptionListTerm>Generation Request Created At</DescriptionListTerm>
-            <DescriptionListDescription>
-              <Timestamp date={request?.creationTime} tooltip={{ variant: TimestampTooltipVariant.default }} />
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-          <DescriptionListGroup>
-            <DescriptionListTerm>Attributes</DescriptionListTerm>
-            <DescriptionListDescription>
-              {request ? (
-                <DescriptionList isHorizontal>
-                  {Object.entries(request).map(([key, value]) => (
-                    <DescriptionListGroup key={key}>
-                      <DescriptionListTerm>{key}</DescriptionListTerm>
-                      <DescriptionListDescription>
-                        <pre style={{ margin: 0 }}>{JSON.stringify(value, null, 2)}</pre>
-                      </DescriptionListDescription>
-                    </DescriptionListGroup>
-                  ))}
-                </DescriptionList>
-              ) : (
-                <span>No request attributes available</span>
-              )}
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-        </DescriptionList>
-      </GridItem>
-    </Grid>
+    <PageSection hasBodyWrapper={false}>
+      <Grid hasGutter span={12}>
+        <GridItem span={12}>
+          <Title headingLevel="h1" size="4xl">
+            Generation Request {id}
+          </Title>
+        </GridItem>
+        <GridItem span={12}>
+          <DescriptionList columnModifier={{ default: '2Col' }}>
+            <DescriptionListGroup>
+              <DescriptionListTerm>ID</DescriptionListTerm>
+              <DescriptionListDescription>
+                <pre>{request.id}</pre>
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+            <DescriptionListGroup>
+              <DescriptionListTerm>Created</DescriptionListTerm>
+              <DescriptionListDescription>
+                <Timestamp date={request.creationTime} tooltip={{ variant: TimestampTooltipVariant.default }} />
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+          </DescriptionList>
+        </GridItem>
+        <GridItem span={12}>
+          <DescriptionList>
+            <DescriptionListGroup>
+              <DescriptionListTerm>Attributes</DescriptionListTerm>
+              <DescriptionListDescription>
+                <CodeBlock>
+                  <CodeBlockCode>{JSON.stringify(request, null, 2)}</CodeBlockCode>
+                </CodeBlock>
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+          </DescriptionList>
+        </GridItem>
+      </Grid>
+    </PageSection>
   );
 };
 
