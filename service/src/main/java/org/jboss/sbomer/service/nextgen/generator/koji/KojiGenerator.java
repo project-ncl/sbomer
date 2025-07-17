0fb719bb045509d6d5358ae38f50b596aa4f94e0
Mert Bicak <mbicak@redhat.com>
2025-07-17 16:28:32 +0200

    feat: nextgen add koji generator to generate brew rpms

---
+++ service/src/main/java/org/jboss/sbomer/service/nextgen/generator/koji/KojiGenerator.java
@@ -0,0 +1,369 @@
+package org.jboss.sbomer.service.nextgen.generator.koji;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.sbomer.core.errors.ApplicationException;
+import org.jboss.sbomer.core.features.sbom.enums.GenerationRequestType;
+import org.jboss.sbomer.core.features.sbom.utils.FileUtils;
+import org.jboss.sbomer.core.features.sbom.utils.MDCUtils;
+import org.jboss.sbomer.service.feature.sbom.config.GenerationRequestControllerConfig;
+import org.jboss.sbomer.service.feature.sbom.k8s.model.SbomGenerationPhase;
+import org.jboss.sbomer.service.feature.sbom.k8s.resources.Labels;
+import org.jboss.sbomer.service.nextgen.controller.tekton.AbstractTektonController;
+import org.jboss.sbomer.service.nextgen.controller.tekton.GenerationTaskRunEventProvider;
+import org.jboss.sbomer.service.nextgen.core.dto.api.GenerationRequest;
+import org.jboss.sbomer.service.nextgen.core.dto.model.GenerationRecord;
+import org.jboss.sbomer.service.nextgen.core.dto.model.ManifestRecord;
+import org.jboss.sbomer.service.nextgen.core.enums.GenerationResult;
+import org.jboss.sbomer.service.nextgen.core.enums.GenerationStatus;
+import org.jboss.sbomer.service.nextgen.core.rest.SBOMerClient;
+import org.jboss.sbomer.service.nextgen.core.utils.JacksonUtils;
+import org.jboss.sbomer.service.nextgen.service.EntityMapper;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.fabric8.kubernetes.api.model.Duration;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.OwnerReferenceBuilder;
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaimVolumeSourceBuilder;
+import io.fabric8.kubernetes.api.model.Quantity;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.tekton.v1beta1.Param;
+import io.fabric8.tekton.v1beta1.ParamBuilder;
+import io.fabric8.tekton.v1beta1.TaskRefBuilder;
+import io.fabric8.tekton.v1beta1.TaskRun;
+import io.fabric8.tekton.v1beta1.TaskRunBuilder;
+import io.fabric8.tekton.v1beta1.TaskRunStepOverride;
+import io.fabric8.tekton.v1beta1.TaskRunStepOverrideBuilder;
+import io.fabric8.tekton.v1beta1.WorkspaceBindingBuilder;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ApplicationScoped
+public class KojiGenerator extends AbstractTektonController {
+
+    public static final String PARAM_BREW_BUILD_ID = "build-id";
+
+    public static final String TASK_SUFFIX = "-generator-brew-rpm";
+    public static final String SA_SUFFIX = "-sa";
+
+    private KojiGenerator() {
+        super(null, null, null, null, null);
+    }
+
+    @Inject
+    public KojiGenerator(
+            @RestClient SBOMerClient sbomerClient,
+            KubernetesClient kubernetesClient,
+            GenerationRequestControllerConfig controllerConfig,
+            ManagedExecutor managedExecutor,
+            EntityMapper mapper) {
+        super(sbomerClient, kubernetesClient, controllerConfig, managedExecutor, mapper);
+    }
+
+    @Override
+    public Set<String> getSupportedTypes() {
+        return Set.of("BREW_RPM");
+    }
+
+    @Override
+    public String getGeneratorName() {
+        return "koji";
+    }
+
+    @Override
+    public String getGeneratorVersion() {
+        return "0.1.0";
+    }
+
+    @Override
+    public void generate(GenerationRecord generationRecord) {
+        log.info("Preparing Tekton Task Run for generation '{}'", generationRecord.id());
+
+        TaskRun desired = desired(generationRecord);
+
+        log.trace("Prepared TaskRun for generation '{}': {}", generationRecord.id(), desired);
+
+        try {
+            kubernetesClient.resources(TaskRun.class).resource(desired).create();
+        } catch (KubernetesClientException e) {
+            log.warn("Unable to schedule Tekton TaskRun", e);
+
+            updateStatus(
+                    generationRecord.id(),
+                    GenerationStatus.FAILED,
+                    GenerationResult.ERR_SYSTEM,
+                    "Unable to schedule Tekton TaskRun: {}",
+                    e.getMessage());
+
+            return;
+        }
+    }
+
+    @Override
+    protected void reconcileGenerating(GenerationRecord generation, Set<TaskRun> relatedTaskRuns) {
+        log.debug("Reconcile '{}' for Generation '{}'...", GenerationStatus.GENERATING, generation.id());
+
+        boolean inProgress = false;
+        boolean success = true;
+        TaskRun erroredTaskRun = null;
+
+        for (TaskRun tr : relatedTaskRuns) {
+
+            if (!isFinished(tr)) {
+                inProgress = true;
+                break;
+            }
+
+            if (!Boolean.TRUE.equals(isSuccessful(tr))) {
+                erroredTaskRun = tr;
+                inProgress = false;
+                success = false;
+                break;
+            }
+        }
+
+        // Still in progress
+        if (inProgress) {
+            log.info("Generation '{}' is still in progress", generation.id());
+            return;
+        }
+
+        if (!success) {
+            String detailedFailureMessage = getDetailedFailureMessage(erroredTaskRun);
+            updateStatus(
+                    generation.id(),
+                    GenerationStatus.FAILED,
+                    GenerationResult.ERR_GENERAL,
+                    "Generation failed, the TaskRun returned failure: {}",
+                    detailedFailureMessage);
+
+            return;
+        }
+
+        // Construct the path to the working directory of the generator
+        Path generationDir = Path.of(controllerConfig.sbomDir(), generation.id());
+
+        log.debug("Reading manifests from '{}'...", generationDir.toAbsolutePath());
+
+        List<Path> manifestPaths;
+
+        try {
+            manifestPaths = FileUtils.findManifests(generationDir);
+        } catch (IOException e) {
+            log.error("Unexpected IO exception occurred while trying to find generated manifests", e);
+
+            updateStatus(
+                    generation.id(),
+                    GenerationStatus.FAILED,
+                    GenerationResult.ERR_SYSTEM,
+                    "Generation succeeded, but reading generated SBOMs failed due IO exception. See logs for more information.");
+
+            return;
+        }
+
+        if (manifestPaths.isEmpty()) {
+            log.error("No manifests found, this is unexpected");
+
+            updateStatus(
+                    generation.id(),
+                    GenerationStatus.FAILED,
+                    GenerationResult.ERR_SYSTEM,
+                    "Generation succeed, but no manifests could be found. At least one was expected. See logs for more information.");
+            return;
+        }
+
+        // Read manifests
+        List<JsonNode> boms;
+
+        try {
+            boms = JacksonUtils.readBoms(manifestPaths);
+        } catch (Exception e) {
+            log.error("Unable to read one or more manifests", e);
+
+            updateStatus(
+                    generation.id(),
+                    GenerationStatus.FAILED,
+                    GenerationResult.ERR_SYSTEM,
+                    "Generation succeeded, but reading generated manifests failed was not successful. See logs for more information.");
+
+            return;
+        }
+
+        // TODO: Validate manifests
+
+        // Store manifests
+        List<ManifestRecord> manifests;
+
+        try {
+            manifests = storeBoms(generation, boms);
+        } catch (ValidationException e) {
+            // There was an error when validating the entity, most probably the SBOM is not valid
+            log.error("Unable to validate generated SBOMs: {}", e.getMessage(), e);
+
+            updateStatus(
+                    generation.id(),
+                    GenerationStatus.FAILED,
+                    GenerationResult.ERR_SYSTEM,
+                    "Generation failed. One or more generated SBOMs failed validation: {}. See logs for more information.",
+                    e.getMessage());
+
+            return;
+        }
+
+        try {
+            // syftImageController.performPost(sboms); // TODO: add this back
+        } catch (ApplicationException e) {
+            updateStatus(generation.id(), GenerationStatus.FAILED, GenerationResult.ERR_POST, e.getMessage());
+            return;
+        }
+
+        updateStatus(
+                generation.id(),
+                GenerationStatus.FINISHED,
+                GenerationResult.SUCCESS,
+                "Generation finished successfully");
+
+    }
+
+    private TaskRunStepOverride resourceOverrides(GenerationRequest request) {
+
+        return new TaskRunStepOverrideBuilder().withName("generate")
+                .withNewResources()
+                .withRequests(
+                        Map.of(
+                                "cpu",
+                                new Quantity(request.generator().config().resources().requests().cpu()),
+                                "memory",
+                                new Quantity(request.generator().config().resources().requests().memory())))
+                .withLimits(
+                        Map.of(
+                                "cpu",
+                                new Quantity(request.generator().config().resources().limits().cpu()),
+                                "memory",
+                                new Quantity(request.generator().config().resources().limits().memory())))
+                .endResources()
+                .build();
+
+    }
+
+    @Override
+    public TaskRun desired(GenerationRecord generation) {
+
+        MDCUtils.removeOtelContext();
+
+        log.debug(
+                "Preparing dependent resource for the '{}' phase related to Generation with id '{}'",
+                SbomGenerationPhase.GENERATE,
+                generation.id());
+
+        // TODO: populate traces when we create generations
+        // TODO: make this a utility maybe
+        Map<String, String> labels = Labels.defaultLabelsToMap(GenerationRequestType.BREW_RPM);
+
+        labels.put(GenerationTaskRunEventProvider.GENERATION_ID_LABEL, generation.id());
+
+        Optional.ofNullable(generation.metadata())
+                .map(meta -> meta.get("otelTraceId"))
+                .filter(JsonNode::isTextual)
+                .map(JsonNode::asText)
+                .ifPresent(traceId -> labels.put(Labels.LABEL_OTEL_TRACE_ID, traceId));
+
+        Optional.ofNullable(generation.metadata())
+                .map(meta -> meta.get("otelSpanId"))
+                .filter(JsonNode::isTextual)
+                .map(JsonNode::asText)
+                .ifPresent(traceId -> labels.put(Labels.LABEL_OTEL_SPAN_ID, traceId));
+
+        Optional.ofNullable(generation.metadata())
+                .map(meta -> meta.get("otelTraceParent"))
+                .filter(JsonNode::isTextual)
+                .map(JsonNode::asText)
+                .ifPresent(traceId -> labels.put(Labels.LABEL_OTEL_TRACEPARENT, traceId));
+
+        GenerationRequest request = JacksonUtils.parse(GenerationRequest.class, generation.request());
+
+        Duration timeout;
+
+        try {
+            timeout = Duration.parse("6h");
+        } catch (ParseException e) {
+            throw new ApplicationException("Cannot set timeout", e);
+        }
+
+        String taskSuffix = null;
+        List<Param> params = new ArrayList<>();
+
+        // Select Tekton Task and set parameters depending on the type of the target
+        switch (request.target().type()) {
+            case "BREW_RPM":
+                taskSuffix = TASK_SUFFIX;
+
+                params.add(
+                        new ParamBuilder().withName(PARAM_BREW_BUILD_ID)
+                                .withNewValue(request.target().identifier())
+                                .build());
+                break;
+
+            default:
+                break;
+        }
+
+        if (taskSuffix == null) {
+            throw new ApplicationException("Unknown target type: {}", request.target().type());
+        }
+
+        TaskRun taskRun = new TaskRunBuilder().withNewMetadata()
+                .withLabels(labels)
+                .withName(
+                        "generation-" + generation.id().toLowerCase() + "-" + SbomGenerationPhase.GENERATE.ordinal()
+                                + "-" + SbomGenerationPhase.GENERATE.name().toLowerCase())
+                .endMetadata()
+                .withNewSpec()
+                .withServiceAccountName(release + SA_SUFFIX)
+                .withTimeout(timeout)
+                .withParams(params)
+                .withTaskRef(new TaskRefBuilder().withName(release + TASK_SUFFIX).build())
+                .withStepOverrides(resourceOverrides(request))
+                .withWorkspaces(
+                        new WorkspaceBindingBuilder().withSubPath(generation.id())
+                                .withName("data")
+                                .withPersistentVolumeClaim(
+                                        new PersistentVolumeClaimVolumeSourceBuilder().withClaimName(release + "-sboms")
+                                                .build())
+                                .build())
+                .endSpec()
+                .build();
+        Deployment deployment = kubernetesClient.apps().deployments().withName(release + "-service").get();
+
+        if (deployment != null) {
+            log.debug("Setting SBOMer deployment as the owner for the newly created TaskRun");
+
+            taskRun.getMetadata()
+                    .setOwnerReferences(
+                            Collections.singletonList(
+                                    new OwnerReferenceBuilder().withKind(HasMetadata.getKind(Deployment.class))
+                                            .withApiVersion(HasMetadata.getApiVersion(Deployment.class))
+                                            .withName(release + "-service")
+                                            .withUid(deployment.getMetadata().getUid())
+                                            .build()));
+        }
+        return taskRun;
+    }
+
+}
