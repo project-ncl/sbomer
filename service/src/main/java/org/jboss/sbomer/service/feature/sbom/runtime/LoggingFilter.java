3c8e60fe9d774acd161f68d92f1869322b3b6198
Tim Carter <112617649+tecarter94@users.noreply.github.com>
2025-08-05 17:14:38 +1000

    fix(SBOMER-395): Fix LoggingFilter and ClientExceptionMapper tracing (#1590)

--- service/src/main/java/org/jboss/sbomer/service/feature/sbom/runtime/LoggingFilter.java
+++ service/src/main/java/org/jboss/sbomer/service/feature/sbom/runtime/LoggingFilter.java
@@ -19,19 +19,14 @@ package org.jboss.sbomer.service.feature.sbom.runtime;
 
 import java.io.IOException;
 
-import org.jboss.pnc.api.constants.MDCKeys;
-import org.jboss.pnc.common.log.MDCUtils;
-import org.slf4j.MDC;
+import org.jboss.sbomer.service.feature.sbom.service.LoggingService;
 
-import io.opentelemetry.api.trace.Span;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.core.Request;
-import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.Provider;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Intercepts all requests and logs them
@@ -39,47 +34,20 @@ import lombok.extern.slf4j.Slf4j;
  * @author Andrea Vibelli
  */
 @Provider
-@Slf4j
 public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
-    private static final String REQUEST_EXECUTION_START = "request-execution-start";
+    @Inject
+    LoggingService loggingService;
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        requestContext.setProperty(REQUEST_EXECUTION_START, System.currentTimeMillis());
-        MDCUtils.setMDCFromRequestContext(requestContext);
-        MDCUtils.addMDCFromOtelHeadersWithFallback(requestContext, Span.current().getSpanContext(), false);
-
-        UriInfo uriInfo = requestContext.getUriInfo();
-        Request request = requestContext.getRequest();
-
-        String forwardedFor = requestContext.getHeaderString("X-FORWARDED-FOR");
-        if (forwardedFor != null) {
-            MDC.put(MDCKeys.X_FORWARDED_FOR_KEY, forwardedFor);
-            log.info("Requested {} {}, forwardedFor: {}.", request.getMethod(), uriInfo.getRequestUri(), forwardedFor);
-        } else {
-            log.info("Requested {} {}.", request.getMethod(), uriInfo.getRequestUri());
-        }
+        loggingService.logRequest(requestContext);
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
-
-        Long startTime = (Long) requestContext.getProperty(REQUEST_EXECUTION_START);
-
-        String took;
-        if (startTime == null) {
-            took = "-1";
-        } else {
-            took = Long.toString(System.currentTimeMillis() - startTime);
-        }
-
-        try (MDC.MDCCloseable mdcTook = MDC.putCloseable(MDCKeys.REQUEST_TOOK, took);
-                MDC.MDCCloseable mdcStatus = MDC
-                        .putCloseable(MDCKeys.RESPONSE_STATUS, Integer.toString(responseContext.getStatus()))) {
-            log.info("Completed {}, took: {}ms.", requestContext.getUriInfo().getPath(), took);
-        }
+        loggingService.logResponse(requestContext, responseContext);
     }
 
 }
