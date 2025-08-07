1fa946bc4459dae781f126cf085728b6d7544d9e
Tomas Havlicek <thavlice@ibm.com>
2025-08-07 14:04:54 +0200

    feat: improve no results and error section

--- ui/src/appV2/components/Sections/NoResultsSection/NoResultSection.tsx
+++ ui/src/appV2/components/Sections/NoResultsSection/NoResultSection.tsx
@@ -1,15 +1,15 @@
-import { Tile, Grid, Column } from '@carbon/react';
+import { Tile } from '@carbon/react';
 import React from 'react';
 
 export const NoResultsSection = () => {
   return (
     <Tile>
-      <Grid>
-        <Column sm={4} md={8} lg={16}>
-          <h3 style={{ marginBottom: '1rem' }}>No Results Found</h3>
-          <p>No results found for the applied filters.</p>
-        </Column>
-      </Grid>
+      <h3>
+        No Results Found
+      </h3>
+      <p>
+        No results found for the applied filters.
+      </p>
     </Tile>
   );
 };
