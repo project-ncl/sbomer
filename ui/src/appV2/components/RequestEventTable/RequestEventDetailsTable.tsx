267b36b788e535dc64cc0c1b33cca46bf1d7d477
Tomas Havlicek <thavlice@ibm.com>
2025-08-07 14:45:50 +0200

    feat: improve visuals of detail pages

--- ui/src/appV2/components/RequestEventTable/RequestEventDetailsTable.tsx
+++ ui/src/appV2/components/RequestEventTable/RequestEventDetailsTable.tsx
@@ -11,6 +11,7 @@ import {
   StructuredListCell,
   Content,
   Heading,
+  Stack,
 } from '@carbon/react';
 
 import React from 'react';
@@ -35,45 +36,53 @@ export const RequestEventDetailsTable = () => {
 
   return (
     <Content>
-      <Grid>
-        <Column sm={4} md={8} lg={16}>
-          <Heading style={{ marginBottom: '2rem' }}>
-            Event {id}
-          </Heading>
-        </Column>
+      <Stack gap={7}>
+        <Grid>
+          <Column sm={4} md={8} lg={16}>
+            <Heading>Event {id}</Heading>
+          </Column>
+        </Grid>
 
-        <Column sm={4} md={8} lg={16}>
-          <StructuredListWrapper>
-            <StructuredListHead>
-              <StructuredListRow head>
-                <StructuredListCell head>Property</StructuredListCell>
-                <StructuredListCell head>Value</StructuredListCell>
-              </StructuredListRow>
-            </StructuredListHead>
-            <StructuredListBody>
-              <StructuredListRow>
-                <StructuredListCell>Event ID</StructuredListCell>
-                <StructuredListCell>
-                  <code style={{ fontFamily: 'monospace' }}>{id}</code>
-                </StructuredListCell>
-              </StructuredListRow>
-              <StructuredListRow>
-                <StructuredListCell>Event Received At</StructuredListCell>
-                <StructuredListCell>
-                  {request.created ? request.created.toISOString() : 'N/A'}
-                </StructuredListCell>
-              </StructuredListRow>
-            </StructuredListBody>
-          </StructuredListWrapper>
-        </Column>
+        <Grid>
+          <Column sm={4} md={8} lg={16}>
+            <StructuredListWrapper>
+              <StructuredListHead>
+                <StructuredListRow head>
+                  <StructuredListCell head>Property</StructuredListCell>
+                  <StructuredListCell head>Value</StructuredListCell>
+                </StructuredListRow>
+              </StructuredListHead>
+              <StructuredListBody>
+                <StructuredListRow>
+                  <StructuredListCell>Event ID</StructuredListCell>
+                  <StructuredListCell>
+                    <CodeSnippet type="inline" hideCopyButton>
+                      {id}
+                    </CodeSnippet>
+                  </StructuredListCell>
+                </StructuredListRow>
+                <StructuredListRow>
+                  <StructuredListCell>Event Received At</StructuredListCell>
+                  <StructuredListCell>
+                    {request.created ? request.created.toISOString() : 'N/A'}
+                  </StructuredListCell>
+                </StructuredListRow>
+              </StructuredListBody>
+            </StructuredListWrapper>
+          </Column>
+        </Grid>
 
-        <Column sm={4} md={8} lg={16} style={{ marginTop: '2rem' }}>
-          <Heading style={{ marginBottom: '1rem' }}>Attributes</Heading>
-          <CodeSnippet type="multi">
-            {JSON.stringify(request, null, 2)}
-          </CodeSnippet>
-        </Column>
-      </Grid>
+        <Grid>
+          <Column sm={4} md={8} lg={16}>
+            <Stack gap={5}>
+              <Heading>Attributes</Heading>
+              <CodeSnippet type="multi">
+                {JSON.stringify(request, null, 2)}
+              </CodeSnippet>
+            </Stack>
+          </Column>
+        </Grid>
+      </Stack>
     </Content>
   );
 };
