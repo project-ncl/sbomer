06915892c51c040b5181e4bc13f2491536ae310e
Tomas Havlicek <thavlice@ibm.com>
2025-08-07 15:11:58 +0200

    fix: change document title to events

--- ui/src/appV2/components/Pages/RequestEvents/RequestEventsPage.tsx
+++ ui/src/appV2/components/Pages/RequestEvents/RequestEventsPage.tsx
@@ -1,11 +1,11 @@
 import { AppLayout } from '@appV2/components/Pages/AppLayout/AppLayout';
 import { RequestEventTable } from '@appV2/components/RequestEventTable/RequestEventTable';
 import { useDocumentTitle } from '@appV2/utils/useDocumentTitle';
-import { Column, Grid, Stack } from '@carbon/react';
+import { Stack } from '@carbon/react';
 import * as React from 'react';
 
 export function RequestEventsPage() {
-  useDocumentTitle('SBOMer | Request Events');
+  useDocumentTitle('SBOMer | Events');
 
   return (
     <AppLayout>
