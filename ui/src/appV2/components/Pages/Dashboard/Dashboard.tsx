304f2f7a50cb7b7ed038ab661745b4769e6cf89e
Tomas Havlicek <thavlice@ibm.com>
2025-08-08 13:15:19 +0200

    feat: simplify dashboard

--- ui/src/appV2/components/Pages/Dashboard/Dashboard.tsx
+++ ui/src/appV2/components/Pages/Dashboard/Dashboard.tsx
@@ -1,54 +1,29 @@
-import { AboutSection } from '@appV2/components/Sections/AboutSection/AboutSection';
-import * as React from 'react';
-import { useDocumentTitle } from '@appV2/utils/useDocumentTitle';
 import { AppLayout } from '@appV2/components/Pages/AppLayout/AppLayout';
+import { AboutSection } from '@appV2/components/Sections/AboutSection/AboutSection';
+import { useDocumentTitle } from '@appV2/utils/useDocumentTitle';
 import {
-  Column,
-  Grid,
-  InlineNotification,
-  Content,
   Heading,
-  Stack,
+  InlineNotification,
+  Stack
 } from '@carbon/react';
+import * as React from 'react';
 
 const Dashboard: React.FunctionComponent = () => {
   useDocumentTitle("SBOMER | Dashboard");
 
   return (
-      <AppLayout>
-        <Content>
-          <Stack gap={7}>
-            <Grid>
-              <Column sm={4} md={8} lg={16}>
-                <Stack gap={4}>
-                  <Heading>
-                    SBOMer Next Generation
-                  </Heading>
-                </Stack>
-              </Column>
-            </Grid>
-
-            <Grid>
-              <Column sm={4} md={8} lg={16}>
-                <InlineNotification
-                  kind="info"
-                  title="In Development"
-                  subtitle="Features are actively being developed and may change."
-                  hideCloseButton
-                />
-              </Column>
-            </Grid>
-
-            <Grid>
-              <Column sm={4} md={8} lg={6}>
-                <AboutSection />
-              </Column>
-            </Grid>
-
-
-          </Stack>
-        </Content>
-      </AppLayout>
+    <AppLayout>
+      <Stack gap={7}>
+        <Heading>SBOMer</Heading>
+        <InlineNotification
+          kind="info"
+          title="In Development"
+          subtitle="Features are actively being developed and may change."
+          hideCloseButton
+        />
+        <AboutSection />
+      </Stack>
+    </AppLayout>
   );
 };
 
