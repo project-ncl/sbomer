7b288a2c1357dba828f52def601f492db6429892
Tomas Havlicek <thavlice@ibm.com>
2025-08-08 13:15:19 +0200

    feat: simplify dashboard

--- ui/src/appV2/components/Sections/AboutSection/AboutSection.tsx
+++ ui/src/appV2/components/Sections/AboutSection/AboutSection.tsx
@@ -1,35 +1,40 @@
-import { Button, Column, Grid, Tile, Stack } from '@carbon/react';
+import { Button, Tile, Stack, Heading, ButtonSet } from '@carbon/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const AboutSection = () => {
   return (
     <Tile>
-      <Stack gap={6}>
+      <Stack gap={5}>
         <div>
-          <h3>About</h3>
+          <Heading>About SBOMer</Heading>
           <p>
             A service to generate <strong>manifests</strong> in the <strong>CycloneDX</strong> format for products.
           </p>
         </div>
-        <Grid>
-          <Column lg={16} md={8} sm={4}>
-            <Button
-              kind="primary"
-              size="lg"
-              as={Link}
-              to="generations"
-            >
-              Generations
-            </Button>
-            <Button kind="secondary" size="lg" as={Link} to="manifests">
-              Manifests
-            </Button>
-            <Button kind="tertiary" size="lg" as={Link} to="events">
-              Events
-            </Button>
-          </Column>
-        </Grid>
+        <ButtonSet>
+          <Button
+            kind="primary"
+            as={Link}
+            to="generations"
+          >
+            Generations
+          </Button>
+          <Button
+            kind="secondary"
+            as={Link}
+            to="manifests"
+          >
+            Manifests
+          </Button>
+          <Button
+            kind="tertiary"
+            as={Link}
+            to="events"
+          >
+            Events
+          </Button>
+        </ButtonSet>
       </Stack>
     </Tile>
   );
